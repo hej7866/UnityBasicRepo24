34cb81f61a7e3686017cf683218186f57f29e23b
hej7866 <hej7866@naver.com>
2024-11-24 21:42:59 +0900

    (2024.11.22) Prefab에 대한 이해 및 프리팹을 이용해 몬스터를 소환하는 코드 구현

---
+++ UnityBasicLearn2024/Assets/Scripts/SpawnAnimal.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityBasic.Prototype2
+{
+    public class SpawnAnimal : MonoBehaviour
+    {
+        [Header("몬스터 소환 세팅")]
+        public GameObject animalPrefab;
+        public float spawnTime = 3f;
+        private float checkTime = 0f;
+
+        public int limitX = 20; 
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Instantiate(animalPrefab);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // 대기 시간
+
+            SpawnAnimals();
+
+        }
+
+        private void SpawnAnimals()
+        {
+            checkTime += Time.deltaTime;
+
+            if (checkTime >= spawnTime)
+            {
+                checkTime = 0f;
+
+                int randomX = Random.Range(-limitX, limitX);
+
+                Vector3 spawnPos = new Vector3(randomX, 0, 19);
+
+                Instantiate(animalPrefab, spawnPos, animalPrefab.transform.rotation);
+            }
+        }
+    }
+}
